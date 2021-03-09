376e29dcb8b09f6f20ac7e116c9a87ec279bfa2e
MaciejSzcz <yetek.lol@gmail.com>
2021-03-09 13:08:18 +0100

    addeed tests and implementations for post, update, delete

--- index.js
+++ index.js
@@ -1,8 +1,70 @@
-const app = require("express")()
+const express = require("express")
+const app = express()
 const port = process.env.PORT || 3000
 
+app.use(express.json())
+
+let data = [
+    { id: 1, title: 'Create a project', completed: true,},
+    { id: 2, title: 'Take a cofféé', completed: true,},
+];
+
 app.get('/', (req, res) => {
-    res.status(200).send("Hello from my docker app running on travis")
+    res.status(200).json(data)
+})
+
+app.post('/', (req, res) => {
+    const itemIds = data.map(item => item.id)
+
+    const newId = itemIds.length > 0 ? Math.max.apply(Math, itemIds) + 1 : 1
+
+    const newItem = {
+        id: newId,
+        title: req.body.title,
+        completed: false,
+    }
+
+    data = [...data, newItem]
+
+    res.status(201).json(newItem)
+})
+
+app.put('/:id', (req, res) => {
+    const found = data.find(item => {
+        return item.id === parseInt(req.params.id)
+    })
+
+    if(found){
+        let updatedItem = {
+            id: found.id,
+            title: req.body.title,
+            completed: req.body.completed,
+        }
+
+        const itemIndex = data.indexOf(found)
+
+        data.splice(itemIndex, 1, updatedItem)
+
+        res.sendStatus(204)
+    } else {
+        res.sendStatus(404)
+    }
+})
+
+app.delete('/:id', (req, res) => {
+    const found = data.find(item => {
+        return item.id === parseInt(req.params.id)
+    })
+
+    if(found){
+        const itemIndex = data.indexOf(found)
+
+        data.splice(itemIndex, 1)
+
+        res.sendStatus(204)
+    } else {
+        res.sendStatus(404)
+    }
 })
 
 app.listen(port, () => {
--- test/server.spec.js
+++ test/server.spec.js
@@ -6,12 +6,46 @@ chai.use(chaiHttp);
 
 
 describe('app', function(){
-    it('should work', function(){ 
+    it('get should work', function(){ 
         chai.request(app)
             .get('/')
             .end(function (err, res) {
                 expect(err).to.be.null;
                 expect(res).to.have.status(200);
+            });
+    })
+
+    it('post should work', function(){ 
+        chai.request(app)
+            .post('/')
+            .send({
+                'title': 'finish react project'
+            })
+            .end(function (err, res) {
+                expect(err).to.be.null;
+                expect(res).to.have.status(201);
+            });
+    })
+
+    it('put should work', function(){ 
+        chai.request(app)
+            .put('/1')
+            .send({
+                'title': 'finish the project',
+                'completed': false
+            })
+            .end(function (err, res) {
+                expect(err).to.be.null;
+                expect(res).to.have.status(204);
+            });
+    })
+
+    it('delete should work', function(){ 
+        chai.request(app)
+            .delete('/2')
+            .end(function (err, res) {
+                expect(err).to.be.null;
+                expect(res).to.have.status(204);
             });
     })
 })
